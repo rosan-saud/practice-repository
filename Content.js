4f21b2ecae47beb0b22b72fc79153a00a1bbe13c
vishk95 <vishalat1994@gmail.com>
2019-06-15 20:20:59 +0530

    vishal 3 commit

--- Content.js
+++ Content.js
@@ -2,13 +2,13 @@ let content = {
     "names" : [
         "CLIENT SERVER MODEL",
         {"GIT" : [
+            'git config --global user.name "Your Name"',
+            'git config --global user.email "you@example.com"'
             "git init", 
             "git remote add origin https://github.com/santo-cold/NodePractice.git",
             "git add -A", 
             "'git commit -am 'Second Commit'",
             "git push origin",
-            'git config --global user.name "Your Name',
-            'git config --global user.email "you@example.com"'
         ]},
         "Package.json",
         {"Install Node Modules and Use them," : [
