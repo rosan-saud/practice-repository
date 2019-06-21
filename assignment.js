bb602feb13a762c4dbc4b7586b7e299751a07f76
rosan-saud <rosansaud5@gmail.com>
2019-06-21 14:49:13 +0530

    this is my first commit

--- assignment.js
+++ assignment.js
@@ -1,9 +1,20 @@
-function nested_fun(val1,val2,val3){
-var concatinated=val1(val2,val3)
-return concatinated
-}
-function fullname(name,surname){
-    return name.concat(surname)
-}
-var data={"firstname":"rosan","lastname":"saud"}
-console.log(nested_fun(fullname,data.firstname,data.lastname))
+// function nested_fun(val1,val2,val3){
+// var concatinated=val1(val2,val3)
+// return concatinated
+// }
+// function fullname(name,surname){
+//     return name.concat(surname)
+// }
+// var data={"firstname":"rosan","lastname":"saud"}
+// console.log(nested_fun(fullname,data.firstname,data.lastname))
+
+var express = require("express")
+var app = express();
+var port = 3000
+app.get('/', function (request, response) {
+    response.send("i dont know what to do at'/'");
+
+      });
+       app.listen(port, function () {
+        console.log('i am all dizzy')
+    })
